fd8df6abe7d6d88ce6ba89909076a28385eabd4b
zhabinka <sergeykarpuk@yandex.by>
2018-11-07 01:09:47 +0300

    Added test #000000 value

--- __tests__/convert-a-hex-string-to-rgb.test.js
+++ __tests__/convert-a-hex-string-to-rgb.test.js
@@ -1,5 +1,10 @@
 import rgbToHex from '../src/convert-a-hex-string-to-rgb';
 
+test('hex to rgb 0', () => {
+  const rgb0 = { r: 0, g: 0, b: 0 };
+  expect(rgbToHex('#000000')).toEqual(rgb0);
+});
+
 test('hex to rgb 1', () => {
   const rgb1 = { r: 255, g: 153, b: 51 };
   expect(rgbToHex('#FF9933')).toEqual(rgb1);
