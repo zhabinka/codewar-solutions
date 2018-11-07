6fc70bec780defeba6dd6dbc320df916115bf3b5
zhabinka <sergeykarpuk@yandex.by>
2018-11-07 21:54:19 +0300

    Refactored

--- src/int32-to-ipv4.js
+++ src/int32-to-ipv4.js
@@ -1,14 +1,10 @@
 // https://www.codewars.com/kata/int32-to-ipv4
 
-const int32ToIp = (code) => {
-  const ipStr = code.toString(2);
-
-  return '0'
-    .repeat(32 - ipStr.length)
-    .concat(ipStr)
-    .match(/.{1,8}/g)
-    .map(el => parseInt(el, 2))
-    .join('.');
-};
+const int32ToIp = code => code
+  .toString(2)
+  .padStart(32, 0)
+  .match(/.{1,8}/g)
+  .map(el => parseInt(el, 2))
+  .join('.');
 
 export default int32ToIp;
