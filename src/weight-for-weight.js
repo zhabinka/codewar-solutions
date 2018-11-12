90a6d54b3e90b19cc05b79a7bb619d997bb10b47
zhabinka <sergeykarpuk@yandex.by>
2018-11-12 12:19:16 +0300

    Order weight refactored

--- src/weight-for-weight.js
+++ src/weight-for-weight.js
@@ -2,19 +2,11 @@
 
 import sumDigit from './sum-digital';
 
-const sortWeight = (a, b) => {
-  if (a.weight === b.weight) {
-    return (a.num).localeCompare(b.num);
-  }
-
-  return a.weight - b.weight;
-};
+const sortWeight = (a, b) => (sumDigit(a) - sumDigit(b) || a.localeCompare(b));
 
 const orderWeight = str => str
   .split(' ')
-  .map(el => ({ num: el, weight: sumDigit(el) }))
   .sort(sortWeight)
-  .reduce((acc, el) => acc.concat(el.num), [])
   .join(' ');
 
 export default orderWeight;
