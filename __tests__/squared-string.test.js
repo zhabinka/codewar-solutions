3e118b711dc90622445c824f7d6fd270991ee5ac
zhabinka <sergeykarpuk@yandex.by>
2018-11-12 23:40:32 +0300

    Square string: rot and selfie added

--- __tests__/squared-string.test.js
+++ __tests__/squared-string.test.js
@@ -1,4 +1,10 @@
-import { oper, vertMirror, horMirror } from '../src/squared-strings/moves-in-squared-strings';
+import {
+  oper,
+  vertMirror,
+  horMirror,
+  rot,
+  selfieAndRot,
+} from '../src/squared-strings/moves-in-squared-strings';
 
 test('Vertical Mirror 1', () => {
   const mirror1 = 'QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw';
@@ -19,3 +25,30 @@ test('Horizontal Mirror 2', () => {
   const mirror4 = 'cEYz\nLPKo\ndbrZ\nnjMK';
   expect(oper(horMirror, 'njMK\ndbrZ\nLPKo\ncEYz')).toBe(mirror4);
 });
+
+test('Rot Mirror 1', () => {
+  const mirror5 = 'BClf\nhkXz\nMfoc\nvKkr';
+  expect(oper(rot, 'rkKv\ncofM\nzXkh\nflCB')).toBe(mirror5);
+});
+
+test('Rot Mirror 2', () => {
+  const mirror6 = 'LyfUVC\nGUFjWk\nABpfre\nJMmPrD\ntcVYqC\nooujif';
+  expect(oper(rot, 'fijuoo\nCqYVct\nDrPmMJ\nerfpBA\nkWjFUG\nCVUfyL')).toBe(mirror6);
+});
+
+test('Selfie and Rot Mirror 1', () => {
+  const mirror7 = 'xZBV....\njsbS....\nJcpN....\nfVnP....\n....PnVf\n....NpcJ\n....Sbsj\n....VBZx';
+  expect(oper(selfieAndRot, 'xZBV\njsbS\nJcpN\nfVnP')).toBe(mirror7);
+});
+
+test('Selfie and Rot Mirror 2', () => {
+  const mirror8 = 'uLcq....\nJkuL....\nYirX....\nnwMB....\n....BMwn\n....XriY\n....LukJ\n....qcLu';
+  expect(oper(selfieAndRot, 'uLcq\nJkuL\nYirX\nnwMB')).toBe(mirror8);
+});
+
+/*
+test('Rot Mirror 1', () => {
+  const mirror5 = '';
+  expect(oper(rot, '')).toBe(mirror5);
+});
+*/
--- src/squared-strings/moves-in-squared-strings.js
+++ src/squared-strings/moves-in-squared-strings.js
@@ -2,7 +2,21 @@
 
 const vertMirror = arr => arr.map(el => [...el].reverse().join(''));
 const horMirror = arr => arr.reverse();
+const selfie = arr => arr.map(el => el.padEnd(el.length * 2, '.'));
+
+const rot = arr => horMirror(vertMirror(arr));
+
+const selfieAndRot = (arr) => {
+  const selfieArr = selfie(arr);
+  return selfieArr.concat(rot(selfieArr));
+};
 
 const oper = (f, data) => f(data.split('\n')).join('\n');
 
-export { oper, vertMirror, horMirror };
+export {
+  oper,
+  vertMirror,
+  horMirror,
+  rot,
+  selfieAndRot,
+};
