aa47559b455997b0fd6a93f14aec6a944ff48a0d
zhabinka <sergeykarpuk@yandex.by>
2018-11-19 23:57:48 +0300

    Scramblies loop using

--- src/scramblies.js
+++ src/scramblies.js
@@ -1,11 +1,28 @@
 // https://www.codewars.com/kata/scramblies/
-
+/*
 const scramble = (chars, str) => {
   if (!chars.includes(str[0])) {
     return str.length === 0;
   }
 
   return scramble(chars.replace(str[0], ''), str.slice(1));
+};
+*/
+const scramble = (chars, string) => {
+  const normalizeStr = str => str.split('').sort().join('');
+
+  let charsSort = normalizeStr(chars);
+  const strSort = normalizeStr(string);
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (!charsSort.includes(strSort[i])) {
+      return false;
+    }
+
+    charsSort = charsSort.replace(strSort[i], '');
+  }
+
+  return true;
 };
 
 export default scramble;
